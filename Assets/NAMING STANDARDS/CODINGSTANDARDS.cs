f98175a23f7862e8e916379b338552f587bc7c55
Icantclutch <54913583+Icantclutch@users.noreply.github.com>
2020-10-23 13:52:08 -0500

    Update CODINGSTANDARDS.cs

--- Assets/NAMING STANDARDS/CODINGSTANDARDS.cs
+++ Assets/NAMING STANDARDS/CODINGSTANDARDS.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class CODINGSTANDARDS
 {
-    private int variable;
-    private float multiWordedVariable;
+    //Public Variables should use camel Case
+    public int variable;
+    public int multiWordVariable;
+
+    //Private Variables are camel case but start with and Underscore;
+    private float _multiWordedVariable;
+    private bool _facts;
+
+    //Function Naming should follow Pascal Case (Title Case) 
+    //Parameter naming should not be confusing with the variable it is meant to reference and should be camel case
+    private void FunctionName(int parameterName)
+    {
+
+    }
+
+    //One or two line comments can be done with "//"
+
+    
+     /* Comments that need multiple lines
+     * or more thorough explanation 
+     * should use this comment format */
+
+
 }
