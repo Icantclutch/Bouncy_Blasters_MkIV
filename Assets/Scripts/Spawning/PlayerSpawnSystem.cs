69e5f78d37b816289baa183adcb74de5fc540bed
hagd13 <55203038+hagd13@users.noreply.github.com>
2020-10-26 13:22:16 -0500

    added comments to spawning scripts

--- Assets/Scripts/Spawning/PlayerSpawnSystem.cs
+++ Assets/Scripts/Spawning/PlayerSpawnSystem.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class PlayerSpawnSystem : NetworkBehaviour
 {
+    //List of all of the spawn points in the scene
     private static List<PlayerSpawnPoint> spawnPoints = new List<PlayerSpawnPoint>();
-    
 
     public static void AddSpawnPoint(PlayerSpawnPoint spawnPoint)
     {
@@ -18,13 +18,19 @@ public class PlayerSpawnSystem : NetworkBehaviour
         spawnPoints.Remove(spawnPoint);
     }
 
+    //Sets the player's position to a chosen spawn point
     public static void SpawnPlayer(GameObject player, bool respawn = true, bool initialSpawn = false)
     {
         if(spawnPoints.Count > 0)
         {
+            //List of acceptable spawnpoints based on boolean parameters and player's team
             List<PlayerSpawnPoint> points = new List<PlayerSpawnPoint>();
+            //Spawn point that will be used
             PlayerSpawnPoint spawnPoint = null;
+
+            //Loop to find acceptable spawn points
             foreach (PlayerSpawnPoint point in spawnPoints) {
+                //Make sure the parameter booleans are the same with the current spawn point
                 if (respawn != point.isRespawnRoom && initialSpawn == point.isStartingPoint)
                 {
                     int playerTeam = player.GetComponent<PlayerHealth>().GetTeam();
@@ -41,11 +47,17 @@ public class PlayerSpawnSystem : NetworkBehaviour
                 }
 
             }
+
+            //Choose a random acceptable spawn point
+            //Temporary, until below loop is made
             if(points.Count > 0)
             {
                 spawnPoint = points[Random.Range(0, points.Count)];
             }
+
             /*
+            //Go though list of acceptable spawn points and choose one
+            //To-do: choose spawn point furthest away from enemy players
             foreach(PlayerSpawnPoint point in points)
             {
                 if (!spawnPoint)
@@ -54,6 +66,7 @@ public class PlayerSpawnSystem : NetworkBehaviour
                 }
             }*/
 
+            //Only set the players transform if a spawn point was found
             if (spawnPoint != null)
             {
                 player.transform.position = spawnPoint.transform.position;
