e5d06bc63d85f03a1e53a77daa09f331f878565b
Mark Daane <49297922+MrUnholyBus@users.noreply.github.com>
2020-10-30 14:22:07 -0500

    Bullet Effects / Wall Particles / Camera Cinematics

---
+++ Assets/Scripts/Effects/CameraTween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTween : MonoBehaviour
+{
+	public Transform camPos;
+	public Transform camLook;
+	public float timeToMove = 1f;
+	public bool blinkStart = false;
+
+    public CameraTween(Transform camPos, Transform camLook, float timeToMove, bool blinkStart)
+    {
+        this.camPos = camPos;
+        this.camLook = camLook;
+        this.timeToMove = timeToMove;
+        this.blinkStart = blinkStart;
+    }
+}
