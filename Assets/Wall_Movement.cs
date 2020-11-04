0f238bf66d7d00a3ad596ea56fab521ac60e9a56
chris Allender <allenderc2849@my.uwstout.edu>
2020-11-04 01:38:42 -0600

    added many more structers to the battery level
    added more walls, indoor and outdoor structures like pillars, angled walls, stagered walls, trees, ect. finally made more adjustments to the positioning of objects to be more precise.

---
+++ Assets/Wall_Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+
+
+public class Wall_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float movementSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        //transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime, 0);
+        //if (transform.position != (-0.6970215, 10.36052, -23.93095)){
+        //    transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime, 0);
+        //}
+        Debug.Log(transform.position);
+    }
+}
