1b1bf4bb85a5f86e9ca0b235fc8c9c9dbc624b6d
Icantclutch <54913583+Icantclutch@users.noreply.github.com>
2020-11-06 11:55:24 -0600

    Map and Hud Changes

--- Assets/Scripts/UI Scripts/PlayerHUD.cs
+++ Assets/Scripts/UI Scripts/PlayerHUD.cs
@@ -10,35 +10,26 @@ public class PlayerHUD : MonoBehaviour
     [SerializeField]
     private Text _batteryCountText;
 
-    private Weapon wep;
-
     [SerializeField]
     private Text _reserveBatteryCountText;
-   
 
     [SerializeField]
     private Text _teamAScoreText;
-    private int _teamAScore;
-
+    
     [SerializeField]
     private Text _teamBScoreText;
-    private int _teamBScore;
-
+  
     [SerializeField]
     private Text _playerHealthText;
-    private int _playerHealth;
-
+  
+    [SerializeField]
+    private Text _playerWeaponText;
     private NetworkManager _networkManager;
     
     // Start is called before the first frame update
     void Start()
     {
-        
         _networkManager = GameObject.FindGameObjectWithTag("Management").GetComponent<NetworkManager>();
-
-        //_teamAScore = _networkManager.GetComponent<GameManagement>().teamA.teamScore;
-        //_teamBScore = _networkManager.GetComponent<GameManagement>().teamB.teamScore;
-        //_playerHealth = GetComponent<PlayerHealth>().GetCharge();
     }
 
     // Update is called once per frame
@@ -49,7 +40,8 @@ public class PlayerHUD : MonoBehaviour
         _reserveBatteryCountText.text = GetComponent<Shooting>().playerWeapons[GetComponent<Shooting>().currentWeapon].currentReserve.ToString();
         //_teamAScoreText.text = _teamAScore.ToString();
         //_teamBScoreText.text = _teamBScore.ToString();
-        _playerHealthText.text = GetComponent<PlayerHealth>().GetCharge().ToString(); ;
+        _playerHealthText.text = GetComponent<PlayerHealth>().GetCharge().ToString();
+        _playerWeaponText.text = GetComponent<Shooting>().playerWeapons[GetComponent<Shooting>().currentWeapon].weapon.name;
 
     }
 }
