5ffed9c339b08849f456d988683bb47f2a92957c
chris Allender <allenderc2849@my.uwstout.edu>
2020-11-11 18:05:39 -0600

    added changes to battery map

---
+++ Assets/Wall_Movement_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_Movement_Script : MonoBehaviour
+{
+    public float maxHeight;
+    public float minHeight;
+    public int numberOfWallls;
+    public GameObject wallSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
