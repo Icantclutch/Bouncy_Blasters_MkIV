e681914549e29ce44610ab25366fc68756907a44
hagd13 <55203038+hagd13@users.noreply.github.com>
2020-11-20 14:31:27 -0600

    display player names

--- Assets/Scripts/Networking/MyNetworkManager.cs
+++ Assets/Scripts/Networking/MyNetworkManager.cs
@@ -6,14 +6,21 @@ using Steamworks;
 
 public class MyNetworkManager : NetworkManager
 {
+    public List<NetworkConnection> players = new List<NetworkConnection>();
     //Overrides OnServerAddPlayer to also get and set the players Steam Id
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
-
+        
         CSteamID steamId = SteamMatchmaking.GetLobbyMemberByIndex(SteamLobby.lobbyId, numPlayers - 1);
 
         conn.identity.GetComponent<PlayerData>().SetSteamId(steamId.m_SteamID);
+        players.Add(conn);
+    }
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        base.OnServerDisconnect(conn);
+        players.Remove(conn);
     }
 
     public override void OnClientSceneChanged(NetworkConnection conn)
@@ -22,4 +29,15 @@ public class MyNetworkManager : NetworkManager
         conn.identity.GetComponent<PlayerData>().RpcSpawnPlayer();
     }
 
+    public GameObject GetLocalPlayer()
+    {
+        foreach(NetworkConnection conn in players)
+        {
+            if(conn.identity.isLocalPlayer)
+            {
+                return conn.identity.gameObject;
+            }
+        }
+        return null;
+    }
 }
---
+++ Assets/Scripts/Player/PlayerInfoDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class PlayerInfoDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private Text _nameDisplayText;
+
+    private MyNetworkManager _networkManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _networkManager = GameObject.FindGameObjectWithTag("Management").GetComponent<MyNetworkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _nameDisplayText.text = GetComponent<PlayerData>().playerName;
+        int team = GetComponent<PlayerData>().team;
+        GameObject localPlayer = _networkManager.GetLocalPlayer();
+        if (team == 0 || (localPlayer && team != localPlayer.GetComponent<PlayerData>().team))
+        {
+            _nameDisplayText.color = Color.red;
+        }
+        else
+        {
+            _nameDisplayText.color = Color.white;
+        }
+
+    }
+}
