e3b7c278571deb4435924f21eb9e0ecf0ddc436a
Icantclutch <54913583+Icantclutch@users.noreply.github.com>
2021-02-12 16:36:38 -0600

    Temp Walls for testing

---
+++ Assets/Scripts/Environment/TempWallsScripts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempWallsScripts : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] walls;
+    bool state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            state = !state;
+            foreach(Transform w in walls)
+            {
+                w.gameObject.SetActive(state);
+                Debug.Log("Walls change: " + state);
+            }
+        }
+    }
+
+
+}
