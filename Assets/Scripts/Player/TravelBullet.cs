e61b25419a841e828957ad9d505f7853328ec209
Michael <hospm6044@my.uwstout.edu>
2021-02-22 13:26:27 -0600

    temp

--- Assets/Scripts/Player/TravelBullet.cs
+++ Assets/Scripts/Player/TravelBullet.cs
@@ -96,7 +96,7 @@ public class TravelBullet : RaycastBullet
                 else
                 {
                     //Destroy the bullet
-                    //DestroyBullet();
+                    DestroyBullet();
                 }
             }
         }
