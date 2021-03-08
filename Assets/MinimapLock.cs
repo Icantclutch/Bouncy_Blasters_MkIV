d63eb07cddaba1d78ad4fe5b09836aeed0746ebc
chris Allender <allenderc2849@my.uwstout.edu>
2021-03-08 10:34:36 -0600

    minimap changes

--- Assets/MinimapLock.cs
+++ Assets/MinimapLock.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MinimapLock : MonoBehaviour
 {
+    public GameObject cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Camera cam = gameObject.GetComponent<Camera>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cam.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
     }
 }
