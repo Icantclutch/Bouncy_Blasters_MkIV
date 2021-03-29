319815deccc0eea4a6accb0d84dee79523ce0af8
Michael <hospm6044@my.uwstout.edu>
2021-03-29 13:48:39 -0500

    Shmooving Walls

--- Assets/Scripts/Environment/MovingWall.cs
+++ Assets/Scripts/Environment/MovingWall.cs
@@ -2,70 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class MovingWall : MonoBehaviour
+public class MovingWall : NetworkBehaviour
 {
-
+    [SyncVar]
     float clock;
-    public float maxTime;
-    public bool isUp;
-    public float moveDistance;
-    public float speed;
-    Vector3 upMove;
-    Vector3 downMove;
-    // Start is called before the first frame update
-    void Start()
+    [SyncVar]
+    public float SpeedMultiplier;
+
+    public AnimationCurve MovementCycle;
+
+    [SyncVar, Range(0,1)]
+    public float startValue = 0f;
+    [SyncVar]
+    Vector3 startPos;
+    [SyncVar]
+    public Vector3 fullyUp;
+    [SyncVar]
+    public Vector3 fullyDown;
+
+    [Server]
+    private void Start()
     {
-        clock = 0;
-        //if the wall is already up, then downMove will send the wall down, and upMove will return it to its original position
-        if(isUp)
-        {
-            downMove = new Vector3(this.transform.position.x, this.transform.position.y - moveDistance, this.transform.position.z);
-            upMove = this.transform.position;
-        }
-        //if the wall starts down, upMove will make the wall go up, and downMove returns it to its original position
-        else if (!isUp)
-        {
-            downMove = this.transform.position;
-            upMove = new Vector3(this.transform.position.x, this.transform.position.y + moveDistance, this.transform.position.z);
-        }
-
-
+        clock = startValue;
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //clock is a timer going up
-        clock += Time.deltaTime;
-        Debug.Log("up position: " + upMove);
-        Debug.Log("down position: " + downMove);
-        
-        MoveWall();
-
-    }
-
-
-
-    public void MoveWall()
-    { 
-    if (clock >= maxTime)
-    {
-            //if the wall is up, move wall down, change isUp to false and reset the timer
-        if(isUp)
+        if (isServer)
         {
-                this.transform.position = Vector3.MoveTowards(this.transform.position, downMove , speed*Time.deltaTime);
-            isUp = false;
-            clock = 0;
+            clock += Time.deltaTime;
         }
-        //if wall is down then move the wall up, change isUp to true and reset the timer
-        else if (!isUp)
-        {
-                this.transform.position = Vector3.MoveTowards(this.transform.position, upMove, speed * Time.deltaTime);
-            isUp = true;
-            clock = 0;
 
-        }
+        transform.position = startPos + Vector3.Lerp(fullyDown, fullyUp, MovementCycle.Evaluate((clock * SpeedMultiplier) % 1f));
     }
-}
 }
