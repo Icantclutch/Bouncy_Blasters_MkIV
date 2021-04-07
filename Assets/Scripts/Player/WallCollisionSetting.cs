438bc5fb16f4edf13a1de085ac363b09682f9ec0
chris Allender <allenderc2849@my.uwstout.edu>
2021-04-07 12:52:50 -0500

    made changes to the wall script and the controller support

--- Assets/Scripts/Player/WallCollisionSetting.cs
+++ Assets/Scripts/Player/WallCollisionSetting.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class WallCollisionSetting : MonoBehaviour
 {
     public GameObject wallInstance;
-    //public Ray myRay;
-    //private RaycastReflection rayScript;
-
-    //GameObject aimRay = GameObject.Find("RaycastReflection");
-
-    
-    void detectCollision(Collider col)
+    public GameObject normal;
+    public GameObject aim;
+    //public RaycastHit getHit;
+    public GameObject scriptObject;
+    private RaycastReflection rayScript;
+    private void Start()
     {
-        
+        rayScript = scriptObject.GetComponent<RaycastReflection>();
     }
-    
+
+    private void Update()
+    {
+        if(rayScript.hit.collider.name == wallInstance.name)
+        {
+            if(normal.active == true)
+            {
+                normal.SetActive(false);
+                aim.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("laser error. blue wall should have by default set to active unless told otherwise");
+            }
+        }
+
+
+    }
+
+
+
 }
