698ce3caf8d7fdd2d814f245914a36c9b57c35cb
chris Allender <allenderc2849@my.uwstout.edu>
2021-04-28 14:35:12 -0500

    controller support

--- Assets/Scripts/Data/Keybinds.cs
+++ Assets/Scripts/Data/Keybinds.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class Keybinds
 {
     //Weapon keys
-    public static KeyCode PrimaryFire = KeyCode.Mouse0; //Left click primary fire
+    public static KeyCode _primaryFire = KeyCode.Mouse0; //Left click primary fire
     public static KeyCode Zoom = KeyCode.Mouse1; //Rigth click zoom 
     public static KeyCode SecondaryFire = KeyCode.Mouse2; //Middle click secondary fire
     public static KeyCode GrenadeFire = KeyCode.G; //G grenade fire
@@ -21,6 +21,31 @@ public static class Keybinds
     public static string Vertical = "Vertical"; //Forward/Back movement axis
     public static string MouseX = "Mouse X"; //Mouse Horizontal
     public static string MouseY = "Mouse Y"; //Mouse Vertical
+
+
+    //ABXY controls
+    public static KeyCode controlJump = KeyCode.JoystickButton0;
+    public static KeyCode controlCrouch = KeyCode.JoystickButton1;
+    public static KeyCode controlReload = KeyCode.JoystickButton2;
+    public static KeyCode controlSwap = KeyCode.JoystickButton3;
+
+    //start button to see menu
+    public static KeyCode controlStart = KeyCode.JoystickButton7;
+
+    public static bool PrimaryFire(bool hold)
+    {
+       
+        Debug.Log(Input.GetAxis("Right Trigger"));
+        return (((hold) ? Input.GetKey(_primaryFire) : Input.GetKeyDown(_primaryFire)) || //inline if statement
+        (Input.GetAxisRaw("Right Trigger") < 0));
+    }
+    /*
+       public static bool Zoom()
+       {
+
+       }
+
+       */
 }
 
 
